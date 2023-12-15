7a17f3ffa1a8a52b0ef0bd43b6c22ae7ce2c0fd1
ABC-Scripts <abcscriptsfivem@gmail.com>
2023-12-15 23:12:56 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+Author "ABC scripts"
+description "ABC_Disable_gta_radio"
+
+client_script {
+'client.lua',
+}
---
+++ client.lua
@@ -0,0 +1,19 @@
+
+ESX  = nil
+local PlayerData                = {}
+
+
+ESX = exports['es_extended']:getSharedObject()
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+            if IsPedInAnyVehicle(PlayerPedId()) then
+                SetUserRadioControlEnabled(false)
+                if GetPlayerRadioStationName() ~= nil then
+                SetVehRadioStation(GetVehiclePedIsIn(PlayerPedId()),"OFF")
+                end
+        end
+    end
+end)
